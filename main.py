dcfb4813f2e6009ad91a42e737690ff524cde812
TECHTANIC <techtanic@outlook.com>
2022-06-02 07:01:19 +0530

    .

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import re
 import os
 import requests
 import json
+from pandas import *
 
 
 from flask import Flask, jsonify, render_template
@@ -10,27 +11,38 @@ from threading import Thread
 
 app = Flask("Multipy")
 
-
+def xl2dict()->dict:
+    xls = ExcelFile("data/small-cap.xlsx")
+    df = xls.parse(xls.sheet_names[0])
+    # with open("xt.txt","w") as f:
+    #     f.write(json.dumps(df.to_dict(),indent=4))
+    return df.to_dict()
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/getstarted")
 def getstarted():
     return render_template("getstarted.html")
 
+
 @app.route("/disclaimer")
 def disclaimer():
-    
+
     return render_template("disclaimer.html")
 
+
 @app.route("/toc")
-def toc():  
+def toc():
     return render_template("toc.html")
+
 
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
 if __name__ == "__main__":
-    app.run(port=os.getenv("PORT", default=5000))
+    app.run(port=os.getenv("PORT", default=5000),debug=True)
